a6fbbe00d5a5a98b1642d84cfd608e47e3f22941
Evan Leahy <49353272+echolimauw@users.noreply.github.com>
2023-07-24 18:29:22 -0400

    resolved some syntax issues. still some issues

---
+++ portscan1.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import os
+import re
+import socket
+import sys
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ipStr = input("Please enter host ip to scan: ")
+portStr = input("Please enter port number to scan: ")
+
+
+if len(sys.argv) > 3:
+	print("No more than two arguments, please.")
+
+def is_port(portStr):
+	if re.match(r'^\d{1,5}$', portStr):
+		portNum = int(portStr)
+		if portNum <= 65535:
+			return True
+	return False
+
+def is_valid_IPv4(ipStr):
+	if re.match(r'^(\d{1,3}\.){3}\d{1,3}$'):
+		return True
+	else:
+		return False
+
+def portscanner(host, port):
+	if sock.connect_ex((host,port)):
+		print(f"Port {port} is closed")
+	else:
+		print(f"Port {port} is open")
+
+def main():
+
+	if is_valid_IPv4(ipStr):
+		host = ipStr
+	else:
+		print("Please try again with a valid IPv4 address.")
+
+	if is_port(portStr):
+		port = portStr
+	else:
+		print("Please try again with a valid port number.")
+
+	portscanner(host, port)
+
+if __name__ == "__main__":
+	main()
